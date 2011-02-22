88b9b99de1ac431bd22c3fd999f254332c5b079d
jsz <jszpilewski@me.com>
2011-02-22 18:00:27 +0100

    moar screen

--- hellstorm/game/src/menu_scene.cpp
+++ hellstorm/game/src/menu_scene.cpp
@@ -51,43 +51,57 @@ namespace test_game
 //		hs::factory::create_particle_emitter(em, "stars.pex", PE_LOOP, hs::vec3d_make(320, 0, 4.0), true);
 //		hs::entity *fire = hs::factory::create_particle_emitter(em, "cool.pex", PE_LOOP, hs::vec3d_screen_center(1.0), true);
 
-		hs::entity *back = hs::factory::create_sprite(em, "background_noraster.png", hs::vec3d_screen_center(-5.0), hs::anchor_center);
+		hs::entity *back = hs::factory::create_sprite(em, "background.png", hs::vec3d_screen_center(-5.0), hs::anchor_center);
 		back->add<comp_psycho_marker>();
 		
 		hs::entity *sunb = hs::factory::create_sprite(em, 
-													  "sunburst_1.png", 
+													  "sunburst_2.png", 
 													  hs::vec3d_make(rand()%(int)hs::cfg::screen.size.w, rand()%(int)hs::cfg::screen.size.h, -4.5), 
 													  hs::anchor_center);
 		
 		sunb->add<comp_psycho_marker>()->type = e_psycho_type_sunburst; 
 		sunb->get<comp_psycho_marker>()->rot_speed = -60.0;
-		sunb->get<hs::comp::renderable>()->alpha = 0.5;
+		sunb->get<hs::comp::renderable>()->alpha = 0.1;
 		sunb->get<comp_psycho_marker>()->velocity = hs::vec2d_make(13.0, -42.0);
 		
-		sunb = hs::factory::create_sprite(em, 
-										  "sunburst_1.png", 
-										  hs::vec3d_make(rand()%(int)hs::cfg::screen.size.w, rand()%(int)hs::cfg::screen.size.h, -4.6), 
-										  hs::anchor_center);
-		
-		sunb->add<comp_psycho_marker>()->type = e_psycho_type_sunburst;
-		sunb->get<comp_psycho_marker>()->rot_speed = -40.0;
-		sunb->get<comp_psycho_marker>()->velocity = hs::vec2d_make(23.0, 12.0);
-		sunb->get<hs::comp::renderable>()->alpha = 0.5;
-
-		sunb = hs::factory::create_sprite(em, 
-										  "sunburst_2.png", 
-										  hs::vec3d_make(rand()%(int)hs::cfg::screen.size.w, rand()%(int)hs::cfg::screen.size.h, -4.55), 
-										  hs::anchor_center);
-		
-		sunb->add<comp_psycho_marker>()->type = e_psycho_type_sunburst;
-		sunb->get<comp_psycho_marker>()->rot_speed = 50.0;
-		sunb->get<comp_psycho_marker>()->velocity = hs::vec2d_make(-43.0, -12.0);
-		sunb->get<hs::comp::renderable>()->alpha = 0.5;
+//		sunb = hs::factory::create_sprite(em, 
+//										  "sunburst_2.png", 
+//										  hs::vec3d_make(rand()%(int)hs::cfg::screen.size.w, rand()%(int)hs::cfg::screen.size.h, -4.6), 
+//										  hs::anchor_center);
+//		
+//		sunb->add<comp_psycho_marker>()->type = e_psycho_type_sunburst;
+//		sunb->get<comp_psycho_marker>()->rot_speed = -40.0;
+//		sunb->get<comp_psycho_marker>()->velocity = hs::vec2d_make(23.0, 12.0);
+//		sunb->get<hs::comp::renderable>()->alpha = 0.1;
+//
+//		sunb = hs::factory::create_sprite(em, 
+//										  "sunburst_2.png", 
+//										  hs::vec3d_make(rand()%(int)hs::cfg::screen.size.w, rand()%(int)hs::cfg::screen.size.h, -4.55), 
+//										  hs::anchor_center);
+//		
+//		sunb->add<comp_psycho_marker>()->type = e_psycho_type_sunburst;
+//		sunb->get<comp_psycho_marker>()->rot_speed = 50.0;
+//		sunb->get<comp_psycho_marker>()->velocity = hs::vec2d_make(-43.0, -12.0);
+//		sunb->get<hs::comp::renderable>()->alpha = 0.1;
 
 		
 		hs::entity *rast = 0;
-		rast = hs::factory::create_sprite(em, "raster_4.png", hs::vec3d_screen_center(-4.1), hs::anchor_center);
+		rast = hs::factory::create_sprite(em, "raster_3.png", hs::vec3d_screen_center(7.0), hs::anchor_center);
 		rast->get<hs::comp::renderable>()->alpha = 1.0;
+
+		rast = hs::factory::create_sprite(em, "rahmen.png", hs::vec3d_screen_center(9.0), hs::anchor_center);
+
+		rast = hs::factory::create_sprite(em, 
+										  "lower.png", 
+										  hs::vec3d_make(hs::cfg::screen.size.w/2, 0, 4.0),
+										  hs::vec2d_make(0.5, 0.0));
+
+		rast = hs::factory::create_sprite(em, 
+										  "upper.png", 
+										  hs::vec3d_make(hs::cfg::screen.size.w/2, hs::cfg::screen.size.h, 4.0),
+										  hs::vec2d_make(0.5, 1.0));
+
+		
 		
 //		rast = hs::factory::create_sprite(em, "raster_2.png", hs::vec3d_screen_center(-4.0), hs::anchor_center);
 //		rast->get<hs::comp::renderable>()->alpha = 0.3;
